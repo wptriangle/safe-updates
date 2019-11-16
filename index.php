626ffc006240a0c47ac4ed415d48e78dce94c7a9
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-17 00:00:15 +0600

    Added index.php file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Index file
+ *
+ * @since      1.0
+ * @package    Safe Updates
+ * @author     Nahid Ferdous Mohit
+ */
+
+/*
+ * If this file is called directly, abort.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/* Silence is golden */
