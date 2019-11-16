32f8319688ae917b5a8b693af112bdcc9dd0b984
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-16 23:45:51 +0600

    Initiated plugin

---
+++ includes/core.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Core plugin file
+ *
+ * @since      1.0
+ * @package    Safe Updates
+ * @author     Nahid Ferdous Mohit
+ */
+
+/*
+ * If this file is called directly, abort.
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
---
+++ safe-updates.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: Safe Updates
+ * Plugin URI: https://nahid.dev/project/safe-updates
+ * Description: Make sure your theme and plugins are tested before updating WordPress
+ * Author: Nahid Ferdous Mohit
+ * Version: 1.0.0
+ * Author URI: https://nahid.dev
+ * Text Domain: safe-updates
+ *
+ * @since      1.0
+ * @package    Safe Updates
+ * @author     Nahid Ferdous Mohit
+ */
+/*
+ * If this file is called directly, abort.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/*
+ * Call the plugin core file
+ */
+require_once plugin_dir_path( __FILE__ ) . 'includes/core.php';
