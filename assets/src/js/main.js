e9be2024e4d9a62c5878f70f5b90e227ee44d1bf
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-18 22:09:42 +0600

    Added code that finds out if any active plugins are not tested with the target core update version

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -8,3 +8,13 @@
  */
 
 import '../sass/main.scss';
+
+/*
+ * Core updates display location
+ */
+
+( function( $ ) {
+	$( document ).ready( function() {
+		$( '#safe-updates-core' ).prependTo( 'ul.core-updates' );
+	} );
+} )( jQuery );
--- includes/updates/core-updates.php
+++ includes/updates/core-updates.php
@@ -6,3 +6,51 @@
  * @package    Safe Updates
  * @author     Nahid Ferdous Mohit
  */
+
+/*
+ * Core updates display
+ */
+
+function safe_updates_core_updates_display() {
+
+    /* Check if core updates are available */
+
+    $core_updates = get_core_updates();
+	if ( $core_updates ) {
+
+        /* Find the target core update version */
+
+		foreach ( $core_updates as $core_update ) {
+			if ( isset( $core_update->response ) || 'upgrade' == $core_update->response ) {
+
+                $target_core_version = $core_update->version;
+
+                /* Create an array of activated plugins */
+
+                $active_plugins = get_option( 'active_plugins' );
+                $all_plugins = get_plugins();
+                $activated_plugins = array();
+                
+                foreach ( $active_plugins as $active_plugin ) {           
+                    if( isset( $all_plugins[ $active_plugin ] ) ){
+                        array_push( $activated_plugins, $all_plugins[ $active_plugin ] );
+                    }           
+                }
+
+                /* Display a table of plugins which are not tested with the target core update version */
+
+                echo '<table id="safe-updates-core"><tr><th>Plugin Name</th><th>Tested up to</th></tr>';
+                foreach( $activated_plugins as $activated_plugin ) {
+                    if ( safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) && safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) != $target_core_version ) {
+                        echo '<tr>';
+                        echo '<td>' . $activated_plugin[ 'Name' ] . '</td>';
+                        echo '<td>' . safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) . '</td>';
+                        echo '</tr>';
+                    }
+                }
+                echo '</table>';
+			}
+        }
+	}
+}
+add_action( 'core_upgrade_preamble', 'safe_updates_core_updates_display' );
