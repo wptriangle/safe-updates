04e677108b50aadad14edd3bd515abf267d725dd
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-18 22:54:06 +0600

    Made slight changes to markup

--- includes/updates/core-updates.php
+++ includes/updates/core-updates.php
@@ -18,6 +18,10 @@ function safe_updates_core_updates_display() {
     $core_updates = get_core_updates();
 	if ( $core_updates ) {
 
+        /* Start display */
+
+        echo '<div id="safe-updates-core">';
+
         /* Find the target core update version */
 
 		foreach ( $core_updates as $core_update ) {
@@ -39,7 +43,7 @@ function safe_updates_core_updates_display() {
 
                 /* Display a table of plugins which are not tested with the target core update version */
 
-                echo '<table id="safe-updates-core" class="widefat updates-table safe-updates-table"><thead><tr><th class="plugin-title">' . __( 'Plugin Name', 'safe-updates' ) . '</th><th>' . __( 'Tested up to', 'safe-updates' ) . '</th><th>' . __( 'Target core version', 'safe-updates' ) . '</th></tr></thead><tbody>';
+                echo '<table class="widefat updates-table safe-updates-table"><thead><tr><th class="plugin-title">' . __( 'Plugin Name', 'safe-updates' ) . '</th><th>' . __( 'Tested up to', 'safe-updates' ) . '</th><th>' . __( 'Target core version', 'safe-updates' ) . '</th></tr></thead><tbody>';
                 foreach( $activated_plugins as $activated_plugin ) {
                     if ( safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) && safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) != $target_core_version ) {
                         echo '<tr>';
@@ -52,6 +56,8 @@ function safe_updates_core_updates_display() {
                 echo '</tbody></table>';
 			}
         }
+
+        echo '</div>';
 	}
 }
 add_action( 'core_upgrade_preamble', 'safe_updates_core_updates_display' );
