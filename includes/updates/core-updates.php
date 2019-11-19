3458347bd35482f19aaea58c84dbffa2bf34d9f0
nfmohit-wpmudev <nfmohit49@gmail.com>
2019-11-19 21:53:01 +0600

    Started implementing a new design

--- includes/updates/core-updates.php
+++ includes/updates/core-updates.php
@@ -29,25 +29,14 @@ function safe_updates_core_updates_display() {
 
                 $target_core_version = $core_update->version;
 
+                /* Check if the active theme is tested */
+
                 $active_theme = wp_get_theme();
 
-                /* Start theme display */
-
                 if ( safe_updates_tested_up_to( 'theme', $active_theme->get( 'TextDomain' ) ) && safe_updates_tested_up_to( 'theme', $active_theme->get( 'TextDomain' ) ) != $target_core_version ) {
-
-                    echo '<h4>' . __( 'Your active theme is untested with the target WordPress version:' ) . '</h4>';
-
-                    /* Display a table of plugins which are not tested with the target core update version */
-
-                    echo '<table class="widefat updates-table safe-updates-table"><thead><tr><th class="theme-title">' . __( 'Theme Name', 'safe-updates' ) . '</th><th>' . __( 'Tested up to', 'safe-updates' ) . '</th><th>' . __( 'Target core version', 'safe-updates' ) . '</th></tr></thead><tbody>';
-                    echo '<tr>';
-                    echo '<td class="theme-title"><strong>' . $active_theme[ 'Name' ] . '</strong></td>';
-                    echo '<td class="tested-up-to untested">' . safe_updates_tested_up_to( 'theme', $active_theme->get( 'TextDomain' ) ) . '</td>';
-                    echo '<td class="target-core-version">' . $target_core_version . '</td>';
-                    echo '</tr>';
-                    echo '</tbody></table>';
+                    $untested_theme = true;
                 } else {
-                    echo '<h4>' . __( 'Your active theme is tested with the target WordPress version.' ) . '</h4>';
+                    $untested_theme = false;
                 }
 
                 /* Create an array of activated plugins */
@@ -70,6 +59,25 @@ function safe_updates_core_updates_display() {
                     if ( safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) && safe_updates_tested_up_to( 'plugin', $activated_plugin[ 'TextDomain' ] ) != $target_core_version ) {
                         array_push( $untested_plugins, $activated_plugin );
                     }
+                }
+
+                /* Start theme display */
+
+                if ( $untested_theme ) {
+
+                    echo '<h4>' . __( 'Your active theme is untested with the target WordPress version:' ) . '</h4>';
+
+                    /* Display a table of plugins which are not tested with the target core update version */
+
+                    echo '<table class="widefat updates-table safe-updates-table"><thead><tr><th class="theme-title">' . __( 'Theme Name', 'safe-updates' ) . '</th><th>' . __( 'Tested up to', 'safe-updates' ) . '</th><th>' . __( 'Target core version', 'safe-updates' ) . '</th></tr></thead><tbody>';
+                    echo '<tr>';
+                    echo '<td class="theme-title"><strong>' . $active_theme[ 'Name' ] . '</strong></td>';
+                    echo '<td class="tested-up-to untested">' . safe_updates_tested_up_to( 'theme', $active_theme->get( 'TextDomain' ) ) . '</td>';
+                    echo '<td class="target-core-version">' . $target_core_version . '</td>';
+                    echo '</tr>';
+                    echo '</tbody></table>';
+                } else {
+                    echo '<h4>' . __( 'Your active theme is tested with the target WordPress version.' ) . '</h4>';
                 }
 
                 /* Start plugin display */
@@ -97,6 +105,61 @@ function safe_updates_core_updates_display() {
                     }
                     echo '</tbody></table>';
                 }
+
+				?>
+				
+				<div class="safe-updates-box">
+
+					<div class="safe-updates-box-header">
+						<h3 class="safe-updates-box-title"><?php echo __( 'Safe Updates', 'safe-updates' ); ?></h3>
+						<div class="safe-updates-actions-left">
+							<span class="safe-updates-tag safe-updates-tag-warning">+1</span>
+						</div>
+						<div class="safe-updates-actions-right"></div>
+					</div>
+
+					<div class="safe-updates-box-body">
+						<div class="accordion" id="safe-updates-accordion">
+
+							<div class="card">
+								<div class="card-header" id="headingThemes" data-toggle="collapse" data-target="#collapseThemes" aria-expanded="true" aria-controls="collapseThemes">
+									<p>Your active theme has been tested with the target WordPress version.</p>
+								</div>
+							</div>
+
+							<div class="card untested">
+								<div class="card-header" id="headingThemes" data-toggle="collapse" data-target="#collapseThemes" aria-expanded="false" aria-controls="collapseThemes">
+									<p>Your active theme has not been tested with the target WordPress version.</p>
+								</div>
+
+								<div id="collapseThemes" class="collapse safe-updates-box" aria-labelledby="headingThemes"
+									data-parent="#safe-updates-accordion">
+									<div class="card-body safe-updates-box-body">
+										<table class="safe-updates-table">
+											<thead>
+												<tr>
+													<th><?php echo __( 'Theme Name', 'safe-updates' ); ?></th>
+													<th><?php echo __( 'Tested up to', 'safe-updates' ); ?></th>
+													<th><?php echo __( 'Target WordPress version', 'safe-updates' ); ?></th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>Theme</td>
+													<td><i class="fas fa-exclamation-circle"></i> 5.0</td>
+													<td>5.3</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+                <?php
 			}
         }
 
